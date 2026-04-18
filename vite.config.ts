69d3b3d7ddf4f8deca92c6399bcfcf015c19cfc1
Alekhya Induri <alekhyainduri6@gmail.com>
2026-04-18 22:49:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/Meta-AI-Prompt-Composer/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
